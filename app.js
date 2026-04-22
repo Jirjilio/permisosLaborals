2bf189621f124b9e49b9a98bac2cd1140c82309d
Jirjilio <jordidelaiglesia@gmail.com>
2026-04-22 20:00:02 +0200

    Instalados paquetes requeridos para el proyecto.

---
+++ app.js
@@ -0,0 +1,31 @@
+import 'dotenv/config'
+import express from 'express'
+import routesPermisos from './backend/routes/permisos.js'
+import routesUsuarios from './backend/routes/users.js'
+import bodyParser from 'body-parser'
+import dbClient from './backend/config/dbClient.js'
+
+const app = express();
+
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocumentation from './swagger.json' with { type: "json" };
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true }))
+
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocumentation))
+
+app.use('/permisos', routesPermisos)
+app.use('/usuarios', routesUsuarios)
+
+try {
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT, () => console.log('Servidor activo en el puerto '+ PORT))
+} catch (error) {
+    console.error(error)
+}
+
+process.on('SIGINT', async () => {
+    dbClient.cerrarConexion()
+    process.exit(0);
+})
---
+++ backend/models/permisos.js
@@ -0,0 +1,51 @@
+//import ObjectID from "mongodb"
+//import dbClient from "../config/dbClient.js";
+import mongoose from "mongoose";
+import Permiso from "../backend/schemas/permisos.js";
+import usuarios from "../backend/schemas/usuarios.js";
+
+class permisosModelo {
+
+    async create(permiso){
+        try {
+            return await Permiso.create(permiso);
+        } catch (error) {
+            console.error("Error al crear el permiso:", error);
+        }
+    }
+
+    async getAll(){
+        try {
+            return await Permiso.find();
+        } catch (error) {
+            console.error("Error al obtener todos los permisos:", error);
+        }
+    }
+
+    async getOne(id){
+        try {
+            return await Permiso.findById({ _id: new mongoose.Types.ObjectId(id) });
+        } catch (error) {
+            console.error("Error al buscar el permiso por ID:", error);
+        }
+        
+    }
+
+    async update(id, permiso){
+        try {
+            return await Permiso.findOneAndUpdate({ _id: new mongoose.Types.ObjectId(id) }, permiso, { new: true });
+        } catch (error) {
+            console.error("Error al actualizar el permiso:", error);
+        }
+    }
+
+    async delete(id){
+        try {
+            return await Permiso.findOneAndDelete({ _id: new mongoose.Types.ObjectId(id) });
+        } catch (error) {
+            console.error("Error al eliminar el permiso:", error);
+        }
+    }
+}
+
+export default new permisosModelo
